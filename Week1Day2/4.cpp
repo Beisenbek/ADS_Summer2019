ab598e46f702a4bcb2e4e46e14ff86c08f715b17
bsnbk <beysenbek@gmail.com>
2019-06-14 12:37:13 +0600

    upd

--- Week1Day2/4.cpp
+++ Week1Day2/4.cpp
@@ -1,27 +1,67 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+ };
+ 
 class Solution {
-public:
-    ListNode* newHead = NULL;
-    
-    void  swap(ListNode * f, ListNode * s){
-        if(s != NULL){
-            ListNode * t = s->next;
-            s->next = f;
-            swap(s, t);
-        }else{
-            newHead = f;
+    public:
+
+        void print(ListNode * head){
+            ListNode * it = head;
+            while(it->next != NULL){
+                cout << it->val << " ";
+                it = it->next;
+            }
+            if(it != NULL){
+             cout << it->val << " ";
+            }
+            cout << endl;
         }
-    }
-    
-    ListNode* reverseList(ListNode* head) {
-        swap(NULL, head);  
-        return newHead;
-    }
+
+        ListNode * add(ListNode * head, int x){
+            if(head == NULL){
+                head = new ListNode(x);
+            }
+            else {
+                ListNode * it = head;
+                while(it->next != NULL){
+                    it = it->next;
+                }
+                it->next = new ListNode(x);
+            }
+            return head;
+        }
+
+        ListNode* deleteDuplicates(ListNode* head) {
+            if(head == NULL) return head;
+            if(head->next == NULL) return head;
+            if(head->val == head->next->val){
+                head = head->next;
+                return deleteDuplicates(head);
+            }
+            return deleteDuplicates(head->next);
+        }
 };
+
+int main(){
+
+    Solution s;
+    ListNode * head = NULL;
+
+    int a[] = {1, 1, 2, 3, 4, 5, 5};
+
+    for(int i = 0; i < 7; ++i){
+        head = s.add(head, a[i]);
+    }
+
+    s.print(head);
+    s.deleteDuplicates(head);
+    s.print(head);
+
+    return 0;
+}
